0b4748892fcbb13c7a42c023ca6f7cf50dd54279
Gary Miguel <garymm@garymm.org>
2023-06-02 20:39:15 +0000

    use static_assert

--- examples/thrust/version_test.cu
+++ examples/thrust/version_test.cu
@@ -1,15 +1,8 @@
 #include <thrust/version.h>
 
-#include <iostream>
+static_assert(THRUST_MAJOR_VERSION == 2);
+static_assert(THRUST_MINOR_VERSION == 1);
 
 int main() {
-    if (THRUST_MAJOR_VERSION != 2) {
-        std::cerr << "Thrust major version is " << THRUST_MAJOR_VERSION << std::endl;
-        return 1;
-    }
-    if (THRUST_MINOR_VERSION != 1) {
-        std::cerr << "Thrust minor version is " << THRUST_MINOR_VERSION << std::endl;
-        return 1;
-    }
     return 0;
 }
